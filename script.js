38355f7291c8158a64391c2ff84b0df268d657d7
Malpicado <jordanmlp@gmail.com>
2023-09-08 09:35:08 -0500

    Calculo de presupuesto publicitario añadido

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const prospectsInput = document.getElementById("prospects");
 const clientsInput = document.getElementById("clients");
 const resultsSection = document.getElementById("results-section");
 const exchangeRateInput = document.getElementById("exchangeRate");
+const googleAdsBudgetInput = document.getElementById("googleAdsBudget");
+const metaAdsBudgetInput = document.getElementById("metaAdsBudget");
 
 // Casillas de verificación para las herramientas
 const metricoolCheckbox = document.getElementById("metricool");
@@ -28,6 +30,8 @@ brand24TeamCheckbox.addEventListener("change", calculateResults);
 activeCampaignProCheckbox.addEventListener("change", calculateResults);
 activeCampaignPlusCheckbox.addEventListener("change", calculateResults);
 exchangeRateInput.addEventListener("input", calculateResults);
+googleAdsBudgetInput.addEventListener("input", calculateResults);
+metaAdsBudgetInput.addEventListener("input", calculateResults);
 
 function calculateResults() {
   const hours = parseInt(hoursInput.value) || 0;
@@ -87,8 +91,20 @@ function calculateResults() {
   // Convertir el presupuesto anual de herramientas a soles peruanos
   const annualToolCostsInSoles = annualToolCosts * exchangeRate;
 
+  // Obtener los nuevos valores de presupuesto para Ads
+  const googleAdsBudget = parseFloat(googleAdsBudgetInput.value) || 0;
+  const metaAdsBudget = parseFloat(metaAdsBudgetInput.value) || 0;
+
+  // Calcular el presupuesto anual para Ads
+  const annualGoogleAdsBudget = googleAdsBudget * 12;
+  const annualMetaAdsBudget = metaAdsBudget * 12;
+
   // Calcular el presupuesto total anual aproximado en soles peruanos
-  const annualTotalAprox = annualFees + annualToolCostsInSoles;
+  const annualTotalAprox =
+    annualFees +
+    annualToolCostsInSoles +
+    annualGoogleAdsBudget +
+    annualMetaAdsBudget;
 
   // Calcular indicadores
   const costPerLead = annualTotalAprox / prospects;
@@ -96,14 +112,24 @@ function calculateResults() {
 
   // Mostrar resultados en la sección de resultados
   resultsSection.innerHTML = `
+<h3>Proyección de honorarios</h3>
 <p>Honorarios Mensuales: ${fees} soles</p>
 <p>Valor por hora del profesional: 100 soles</p>
-<p>Costo de Herramientas Mensuales: ${toolCosts} dólares</p>
 <p>Presupuesto Anual de Honorarios proyectado: ${annualFees} soles</p>
+<h3>Coste de herramientas</h3>
+<p>Costo de Herramientas Mensuales: ${toolCosts} dólares</p>
 <p>Presupuesto Anual de Herramientas proyectado: ${annualToolCosts} dólares</p>
+<p>Presupuesto Anual de Herramientas proyectado en soles: ${annualToolCostsInSoles.toFixed(2)} Soles</p>
+<h3>Inversión publicitaria anual</h3>
+<p>Presupuesto Mensual Google Ads: ${googleAdsBudget} soles</p>
+<p>Presupuesto Mensual Meta Ads: ${metaAdsBudget} soles</p>
+<p>Presupuesto Anual Google Ads proyectado: ${annualGoogleAdsBudget} soles</p>
+<p>Presupuesto Anual Meta Ads proyectado: ${annualMetaAdsBudget} soles</p>
+<h3>Coste anual del proyecto</h3>
 <p>Presupuesto Anual Aproximado proyectado en Soles: ${annualTotalAprox.toFixed(
     2
   )} Soles</p>
+<h3>Resultados esperados</h3>
 <p>Prospectos Mensuales esperados: ${monthlyProspects}</p>
 <p>Clientes Mensuales esperados: ${monthlyClients}</p>
 <h3>Distribución de Procesos de Admisión</h3>
@@ -120,7 +146,7 @@ function calculateResults() {
       ? "(Válido, Conversión esperada es 80% o más)"
       : "(No Válido, Conversión esperada es menor al 80%)"
   }</p>
-<h3>Indicadores</h3>
+<h3>Indicadores objetivo</h3>
 <p>Coste por Lead esperado: ${costPerLead.toFixed(2)} PEN máximos</p>
 <p>Coste de Adquisición de Cliente esperado: ${costAdquisitionClients.toFixed(
     2
